3c028d819bf05321d89f7ff21bd5e7934c6cf104
Tin Tran <tinhardware@gmail.com>
2022-06-17 11:11:40 +0700

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Tin Tran on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
